0c1888435744925bd9240b12ec954e9e33b0ba0e
Cancerrrr <33804181+Cancerrrr@users.noreply.github.com>
2021-01-25 13:08:39 +0800

    PP_Branch_Lou_12
    Pdf Spawn Adjusted

--- OCC_Project/ui_test/PDF_Spawn.py
+++ OCC_Project/ui_test/PDF_Spawn.py
@@ -1,6 +1,8 @@
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak, Table, TableStyle
+import reportlab
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak, Table, TableStyle, LongTable
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
-from reportlab.lib.units import mm
+from reportlab.lib.units import mm, inch
 from reportlab.lib import colors
 from reportlab.lib.enums import TA_CENTER, TA_LEFT
 from reportlab.pdfbase import pdfmetrics
@@ -12,8 +14,9 @@ pdfmetrics.registerFont(TTFont('hv', 'Helvetica.ttf'))
 # 生成PDF文件
 class PDFGenerator:
     def __init__(self, filename):
+
         self.filename = filename
-        self.file_path = '/'
+        self.file_path = 'F:/pycharm_project/PyOcc_Project_2021/OCC_Project/pdf_spawn/'
         self.title_style = ParagraphStyle(name="TitleStyle", fontName="pingbold", fontSize=48, alignment=TA_LEFT,)
         self.sub_title_style = ParagraphStyle(name="SubTitleStyle", fontName="hv", fontSize=32,
                                               textColor=colors.HexColor(0x666666), alignment=TA_LEFT, )
@@ -27,39 +30,44 @@ class PDFGenerator:
                                                 spaceAfter=10, alignment=TA_LEFT, )
         self.basic_style = TableStyle([('FONTNAME', (0, 0), (-1, -1), 'ping'),
                                        ('FONTSIZE', (0, 0), (-1, -1), 12),
-                                       ('ALIGN', (0, 0), (3, 2), 'CENTER'),
-                                       ('ALIGN', (0, 13), (3, 13), 'CENTER'),
-                                       ('ALIGN', (0, 20), (3, 20), 'CENTER'),
-                                       ('ALIGN', (0, 28), (3, 28), 'CENTER'),
+                                       ('ALIGN', (0, 0), (5, 0), 'CENTER'),
+                                       ('ALIGN', (0, 2), (5, 2), 'CENTER'),
+                                       ('ALIGN', (0, 22), (5, 22), 'CENTER'),
+                                       ('ALIGN', (0, 31), (5, 31), 'CENTER'),
                                        ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
                                        ('BOTTOMPADDING', (0, 0), (-1, -1), 6),
                                        # 'SPAN' (列,行)坐标
-                                       ('SPAN', (2, 0), (3, 0)),
-                                       ('SPAN', (0, 1), (1, 1)),
-                                       ('SPAN', (2, 1), (3, 1)),
-                                       ('SPAN', (0, 2), (1, 2)),
-                                       ('SPAN', (2, 2), (3, 2)),
-                                       ('SPAN', (3, 3), (3, 12)),
+                                       ('SPAN', (3, 0), (5, 0)),
+                                       ('SPAN', (0, 0), (1, 0)),
+                                       ('SPAN', (0, 1), (2, 1)),
+                                       ('SPAN', (3, 1), (5, 1)),
+                                       ('SPAN', (0, 2), (2, 2)),
+                                       ('SPAN', (3, 2), (5, 2)),
+                                       ('SPAN', (3, 3), (5, 3)),
+                                       ('SPAN', (3, 4), (5, 4)),
+                                       ('SPAN', (3, 5), (5, 5)),
+                                       ('SPAN', (1, 5), (2, 5)),
+                                       ('SPAN', (3, 6), (5, 6)),
+                                       ('SPAN', (1, 6), (2, 6)),
+                                       ('SPAN', (3, 7), (5, 7)),
                                        ('SPAN', (1, 7), (2, 7)),
-                                       ('SPAN', (1, 12), (2, 12)),
-                                       ('SPAN', (0, 13), (3, 13)),
-                                       ('SPAN', (1, 14), (3, 14)),
-                                       ('SPAN', (2, 15), (3, 15)),
-                                       ('SPAN', (2, 16), (3, 16)),
-                                       ('SPAN', (2, 17), (3, 17)),
-                                       ('SPAN', (1, 18), (3, 18)),
-                                       ('SPAN', (1, 19), (3, 19)),
-                                       ('SPAN', (0, 20), (3, 20)),
-                                       ('SPAN', (1, 21), (3, 21)),
-                                       ('SPAN', (2, 22), (3, 22)),
-                                       ('SPAN', (2, 23), (3, 23)),
-                                       ('SPAN', (2, 24), (3, 24)),
-                                       ('SPAN', (2, 25), (3, 25)),
-                                       ('SPAN', (1, 26), (3, 26)),
-                                       ('SPAN', (2, 27), (3, 27)),
-                                       ('SPAN', (0, 28), (3, 28)),
-                                       ('SPAN', (2, 29), (3, 29)),
-                                       ('SPAN', (1, 30), (3, 30)),
+                                       ('SPAN', (3, 8), (5, 8)),
+                                       ('SPAN', (3, 9), (5, 9)),
+                                       ('SPAN', (3, 10), (5, 10)),
+                                       ('SPAN', (3, 11), (5, 11)),
+                                       ('SPAN', (3, 12), (5, 12)),
+                                       ('SPAN', (3, 13), (5, 13)),
+                                       ('SPAN', (0, 13), (1, 13)),
+                                       ('SPAN', (3, 3), (5, 13)),
+                                       ('SPAN', (4, 14), (5, 14)),
+                                       ('SPAN', (4, 15), (5, 15)),
+                                       ('SPAN', (4, 16), (5, 16)),
+                                       ('SPAN', (4, 17), (5, 17)),
+                                       ('SPAN', (0, 18), (0, 19)),
+                                       ('SPAN', (1, 18), (1, 19)),
+                                       ('SPAN', (2, 18), (2, 19)),
+                                       ('SPAN', (0, 22), (5, 22)),
+                                       ('SPAN', (0, 31), (5, 31)),
                                        ('GRID', (0, 0), (-1, -1), 0.5, colors.black),
                                        ])
         self.common_style = TableStyle([('FONTNAME', (0, 0), (-1, -1), 'ping'),
@@ -74,7 +82,6 @@ class PDFGenerator:
         story = []
 
         # 首页内容
-        story.append(Spacer(1, 20 * mm))
         '''img = Image('./icons/3/newicon.png')
         img.drawHeight = 20 * mm
         img.drawWidth = 40 * mm
@@ -110,9 +117,9 @@ class PDFGenerator:
 
         # 基础参数
         #story.append(Paragraph("基础参数", self.sub_table_style))
-        basic_table = Table(basic_data, colWidths=[45*mm, 61*mm, 40*mm, 55*mm], rowHeights=12 * mm, style=self.basic_style)
+        basic_table = Table(basic_data, colWidths=None , rowHeights=None, style=self.basic_style)
         story.append(basic_table)
-        story.append(Spacer(1, 10 * mm))
+        #story.append(Spacer(1, 10 * mm))
 
         '''# 测试用例集
         story.append(Paragraph("用例集参数", self.sub_table_style))
@@ -140,43 +147,46 @@ class PDFGenerator:
         p0_case_table.setStyle(self.common_style)
         story.append(p0_case_table)'''
 
-        doc = SimpleDocTemplate(self.file_path + self.filename + ".pdf",
-                                leftMargin=20 * mm, rightMargin=20 * mm, topMargin=20 * mm, bottomMargin=20 * mm)
-
-
+        #doc = SimpleDocTemplate(self.file_path + self.filename + ".pdf",
+                                #leftMargin=20 * mm, rightMargin=20 * mm, topMargin=20 * mm, bottomMargin=20 * mm)
+        doc = SimpleDocTemplate(self.file_path + self.filename + ".pdf", leftSpace=37 * mm, rightMargin=15 * mm, pagesize = A4)
         doc.build(story)
 
 if __name__ == '__main__':
     pdf_generator = PDFGenerator('3DViewer')
-    basic_data = [['封头校核', '计算单位', '压力容器计算软件', ''],
-                ['计算标准', '', 'GB 150', ''],
-                ['计算条件', '', '封头简图', ''],
-                ['计算压力P', '3.10', 'MPa', ''],
-                ['设计温度T', '100.00', '℃', ''],
-                ['内径', '500', 'mm', ''],
-                ['曲面深度 H', '150.00', 'mm', ''],
-                ['材料', 'Q345R', '', ''],
-                ['设计温度许用应力', '189.00', 'MPa', ''],
-                ['试验温度许用应力', '189.00', 'MPa', ''],
-                ['负偏差C1', '0.30', 'mm', ''],
-                ['腐蚀裕量C2', '0.50', 'mm', ''],
-                ['焊接接头系数', '1.00', '', ''],
-                ['压力试验时应力校核', '', '', ''],
-                ['压力试验类型', '液压试验', '', ''],
-                ['试验压力值', '3.2000', 'MPa', ''],
-                ['压力试验允许通过应力', '310.5', 'MPa', ''],
-                ['试验压力下封头应力', '89.28', 'MPa', ''],
-                ['校核条件', 'Q1<Q2', '', ''],
-                ['校核结果', '合格', '', ''],
-                ['厚度及重量计算', '', '', ''],
-                ['形状系数', '0.7963', '', ''],
-                ['计算厚度', '3.28', 'mm', ''],
-                ['有效厚度', '7.20', 'mm', ''],
-                ['最小厚度', '3.00', 'mm', ''],
-                ['名义厚度', '8.00', 'mm', ''],
-                ['结论', '满足最小厚度要求', '', ''],
-                ['重量', '23.79', 'Kg', ''],
-                ['压力计算', '', '', ''],
-                ['最大允许工作压力', '6.7749', 'MPa', ''],
-                ['结论', '合格', '', '']]
+    img = Image('./icons/TTKT.png')
+    img.drawWidth = 4*inch
+    img.drawHeight = 2.5*inch
+    basic_data = [['开孔补强计算', '', '计算单位', '压力容器计算软件', '', ''],
+                ['接管:N1', '', '', '计算方法:ASME VIII-1', '', ''],
+                ['设计条件', '', '', '简图', '', ''],
+                ['计算压力P', '5.8', 'MPa', img, '', ''],
+                ['设计温度T', '100.00', '℃', '', '', ''],
+                ['壳体型式', '圆形筒体', '', '', '', ''],
+                ['壳体材料名称及类型', 'Q245R', '', '', '', ''],
+                ['焊接接头系数E', '1', '', '', '', ''],
+                ['壳体内直径Di', '1000', 'mm', '', '', ''],
+                ['壳体开孔处公称厚度t', '35', 'mm', '', '', ''],
+                ['壳体厚度负偏差C1', '0.00', 'mm', '', '', ''],
+                ['壳体腐蚀裕量C2', '3', 'mm', '', '', ''],
+                ['壳体材料许用应力', '140', 'MPa', '', '', ''],
+                ['接管轴线与筒体表面法线夹角', '', '0', '', '', ''],
+                ['接管实际外伸长度', '300', 'mm', '接管连接型式', '插入式接管壁安放在容器壁上', ''],
+                ['接管实际内伸长度', '0', 'mm', '接管材料', 'Q245R', ''],
+                ['接管壁公称厚度', '1', 'mm', '名称及类型', '板材', ''],
+                ['接管内半径', '3', 'mm', '补强圈材料名称', '', ''],
+                ['接管腐蚀裕量C2t', '', 'mm', '补强圈宽度', '', 'mm'],
+                ['', '', '', '补强圈厚度', '', 'mm'],
+                ['接管厚度负偏差C1t', '0', 'mm', '补强圈厚度负偏差C1r', '', 'mm'],
+                ['接管材料许用应力', '140', 'MPa', '补强圈许用应力', '' ,'mm'],
+                ['开孔补强计算', '', '', '', '', ''],
+                ['沿容器壁补强范围', '71', 'mm', '最大局部一次薄膜应力', '1', 'MPa'],
+                ['伸出在容器外表面沿接管壁补强范围', '21.250', 'mm', '平均一次薄膜应力', '1.3749', 'MPa'],
+                ['伸入在容器内表面沿接管壁补强范围', '0', 'mm', '总体一次薄膜应力', '1', 'MPa'],
+                ['壳体有效半径', '71', 'mm', '壳体有效厚度', '211', 'mm'],
+                ['接管有效外伸长度', '49.85', 'mm', '接管有效内伸长度', '0', 'mm'],
+                ['接管开口处有效总面积', '1502', 'mm^2', '内压在壳体上引起的力', '1519', 'mm'],
+                ['内压在容器外侧接管上引起的力', '3053', 'N', '内压引起的不连续的力', '100', 'mm^2'],
+                ['许用应力', '', 'MPa', '接管最大许用应力', '100', 'mm^2'],
+                ['结论:合格', '', '', '']]
     pdf_generator.genTaskPDF([], [], basic_data, [], [], [])
